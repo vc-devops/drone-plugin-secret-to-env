d30bd86f2ce188bcab0e31093faa5a7c9ebc138e
hieupv <hieupv@codersvn.com>
2020-12-10 12:51:24 +0700

    exit if can not compile file

--- src/services/env.service.ts
+++ src/services/env.service.ts
@@ -3,10 +3,14 @@ import { template } from 'lodash';
 
 export class EnvService {
   async compile(file, template_file_path, values) {
-    const content = fs.readFileSync(template_file_path, 'utf8');
-    const compiled = template(content);
-    const new_content = compiled(values);
-    fs.writeFileSync(file, new_content);
-    return new_content;
+    try {
+      const content = fs.readFileSync(template_file_path, 'utf8');
+      const compiled = template(content);
+      const new_content = compiled(values);
+      fs.writeFileSync(file, new_content);
+      return new_content;
+    } catch (error) {
+      process.exit(1);
+    }
   }
 }
